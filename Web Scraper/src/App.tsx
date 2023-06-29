e106b828fb0953a0363e8ad6a936cdbf5be7343f
Canesp <philip.decanesie@gmail.com>
2023-06-29 23:15:17 +0200

    added mantine

--- Web Scraper/src/App.tsx
+++ Web Scraper/src/App.tsx
@@ -1,10 +1,31 @@
-
+import { AppShell, Header } from '@mantine/core';
+import { Container } from '@mantine/core';
+import { Grid } from '@mantine/core';
+import { Table } from '@mantine/core';
 import "./App.css";
 
 function App() {
 
+  
+
   return (
-    "hej"
+    <AppShell padding="md">
+      
+      <Grid grow gutter="lg" style={{height: "100%"}}>
+
+        <Grid.Col span={6}>
+
+
+
+        </Grid.Col>
+
+        <Grid.Col span={2}>
+
+        </Grid.Col>
+      
+      </Grid>
+    
+    </AppShell>
   );
 }
 
