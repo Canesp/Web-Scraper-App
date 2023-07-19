950f1e72022028375bbc59e5fccb091c8d309a42
Canesp <philip.decanesie@gmail.com>
2023-07-19 12:06:46 +0200

    made it so when press outside of dropdown it close

--- web/index.js
+++ web/index.js
@@ -4,8 +4,8 @@ function checkUrlAuth() {
     var url_input = document.getElementById("url-Input");
     var input_value = url_input.value;
 
-    eel.webAuthenticator(input_value)(function(response) {
-        
+    eel.webAuthenticator(input_value)(function (response) {
+
         if (response == false && input_value != 0) {
             url_input.style.borderColor = "red";
         } else if (response == true) {
@@ -33,3 +33,25 @@ function toggleSelectMenu() {
     select_Menu.classList.toggle("active");
 }
 
+document.addEventListener("click", function (event) {
+
+    const parentElements = document.querySelectorAll(".Multi-Select-Container");
+    const clickedElement = event.target;
+
+    // Check if the clicked element is any of the parent elements or their children
+    for (const parentElement of parentElements) {
+        if (parentElement.contains(clickedElement) || clickedElement.tagName === "A") {
+            // Clicked on 'a' or its children or clicked inside one of the parent elements, do nothing
+            return;
+        }
+
+        // Check if the parent element has the 'active' class indicating it has an active toggle state
+        if (parentElement.classList.contains("active")) {
+            // Parent element is active, call the function.
+            toggleSelectMenu();;
+        }
+    }
+
+    // Clicked outside 'a' and parent elements, returns.
+    return;
+});
